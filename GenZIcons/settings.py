b5a2d6c2d4e754eb5e4ed1752d701e5f123ef418
Ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-08-24 22:05:04 +0100

    want to add a home page

--- GenZIcons/settings.py
+++ GenZIcons/settings.py
@@ -146,6 +146,3 @@ MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
-
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
